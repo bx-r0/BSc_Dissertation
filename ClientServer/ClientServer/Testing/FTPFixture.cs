311d926500cc364641c9a73f31256a8e00705dfe
Aidan Fray <afray@hotmail.co.uk>
2017-10-12 12:20:48 +0100

    Changes; -Testing fixture for FTP functionality added

---
+++ ClientServer/ClientServer/Testing/FTPFixture.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientServer.Testing
+{
+    [TestFixture]
+    class FTPFixture
+    {
+
+        //# Connection
+        [Test]
+        public void FTP_SucessfulConnection()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [Test]
+        public void FTP_UnsucessfullConnection()
+        {
+            //NOTE: This needs to be handled and not throw an exception
+            throw new NotImplementedException();
+        }
+        
+
+        //# Downloading
+        [Test]
+        public void DownloadTextFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void DownloadImageFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void DownloadFolder()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void Download_Invalid_FileDoesNotExist()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //# Uploading
+        [Test]
+        public void UploadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void UploadThenDownload()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //# File management
+        [Test]
+        public void CreateFolder()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void DeleteFolder()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CreateThenDeleteFolder()
+        {
+            throw new NotImplementedException();
+ 
+        }
+    }
+}
