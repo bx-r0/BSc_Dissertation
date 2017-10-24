858493b4b7758c644c8d56b1216d189e36fee7fe
Aidan Fray <afray@hotmail.co.uk>
2017-10-24 13:55:10 +0100

    Changes; -Added back FTPClient - finally

---
+++ ClientServer/ClientServer/FTP/FTPClient.cs
@@ -0,0 +1,75 @@
+﻿using FluentFTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClientServer.FTP
+{
+    class FTPClient
+    {
+        //# Client
+        private FtpClient client;
+
+        //# Connection details 
+        private string address = "http://127.0.0.1:21";
+        private int port = 21;
+        private string mainDir = "/";
+
+
+        //# Constructors
+        public FTPClient() { }
+        public FTPClient(string address)
+        {
+            this.address = address;
+        }
+
+        //# Setup code
+        public void Setup()
+        {
+            //# Creates a new FTP connection
+            client = new FtpClient(address);
+
+            //# Attempts a connection
+            try
+            {
+                client.Connect();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error in connection: " + e.Message);
+            }
+
+        }
+
+
+        //# File code
+
+        //# Upload file
+        public void Upload(string currentFilePath, string FTPFilePath)
+        {
+            try
+            {
+                client.UploadFile(currentFilePath, FTPFilePath);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.InnerException.Message);
+            }
+        }
+
+        //# Download file
+        public void Download()
+        {
+            throw new NotImplementedException();
+        }
+
+        //# Files
+        public FtpListItem[] GetAllListing(string uri)
+        {
+            return client.GetListing(uri);
+        }
+    }
+}
