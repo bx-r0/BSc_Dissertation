69a47e0d4c9a03b0eef7baa35538a24f159f80c1
Aidan Fray <afray@hotmail.co.uk>
2017-10-27 18:09:11 +0000

    Changes; -Added latency mode where packets are delayed before being send on

--- PacketCapture/PacketCapture.py
+++ PacketCapture/PacketCapture.py
@@ -3,17 +3,45 @@
 # "sudo iptables -A INPUT -j NFQUEUE"
 #----------------------------------------#
 
+import time
+from scapy.all import *
 from netfilterqueue import NetfilterQueue
+
 
 def print_packet(packet):
     print("[!] ", end='')
     print(packet)
 
+
+def edit_packet(packet):
+    # Converts into Scapy compatable string
+    pkt = IP(packet.get_payload())
+
+    # ------------------ #
+    # EDIT PACKET HERE
+    # ------------------ #
+
+    # Sets the packet to the modified version
+    packet.set_payload(bytes(pkt))
+
+    # Accepts and lets the packet leave the queue
+    packet.accept()
+
+
+def latency(packet):
+    # Used to debug
+    print_packet(packet)
+
+    # Issues latency of one second
+    time.sleep(1)
+    packet.accept()
+
+
 # Creates the object
 nfqueue = NetfilterQueue()
 
 # 0 is the default NFQUEUE and print_packet is the
-nfqueue.bind(0, print_packet)
+nfqueue.bind(0, latency)
 
 try:
     print("[*] Waiting ")
