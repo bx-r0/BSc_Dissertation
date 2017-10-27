313f3940bad7b97de76510287b5f788e11f2ca11
Aidan Fray <afray@hotmail.co.uk>
2017-10-27 17:34:25 +0000

    Changes; -Small functionality added to PacketCapture to read packets from the NFQUEUE, it takes them from the queue and prints them

--- PacketCapture/PacketCapture.py
+++ PacketCapture/PacketCapture.py
@@ -1,3 +1,22 @@
-import time
+## This iptable command needs to be run to push all packets into the NFQUEUE
+#----------------------------------------#
+# "sudo iptables -A INPUT -j NFQUEUE"
+#----------------------------------------#
 
+from netfilterqueue import NetfilterQueue
 
+def print_packet(packet):
+    print("[!] ", end='')
+    print(packet)
+
+# Creates the object
+nfqueue = NetfilterQueue()
+
+# 0 is the default NFQUEUE and print_packet is the
+nfqueue.bind(0, print_packet)
+
+try:
+    print("[*] Waiting ")
+    nfqueue.run()
+except KeyboardInterrupt:
+    pass
