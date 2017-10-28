e5b902ea5ef7cc4769730bd4ead5185fb3c70eb6
Aidan Fray <afray@hotmail.co.uk>
2017-10-28 19:34:52 +0000

    Changes; -Added packet loss mode that drops packets randomly according to a percentage target -Updated the top comment

--- PacketCapture/PacketCapture.py
+++ PacketCapture/PacketCapture.py
@@ -1,9 +1,15 @@
-## This iptable command needs to be run to push all packets into the NFQUEUE
-#----------------------------------------#
-# "sudo iptables -A INPUT -j NFQUEUE"
-#----------------------------------------#
+'''
+This iptables command needs to be run to push all packets into the NFQUEUE:
+
+        "sudo iptables -A INPUT -j NFQUEUE"
+
+To restore full internet connection run:
+
+        "sudo iptables -F"
+'''
 
 import time
+import random
 from scapy.all import *
 from netfilterqueue import NetfilterQueue
 
@@ -14,7 +20,7 @@ def print_packet(packet):
 
 
 def edit_packet(packet):
-    # Converts into Scapy compatable string
+    # Converts into Scapy compatible string
     pkt = IP(packet.get_payload())
 
     # ------------------ #
@@ -28,22 +34,48 @@ def edit_packet(packet):
     packet.accept()
 
 
-def latency(packet):
+def packet_latency(packet):
     # Used to debug
     print_packet(packet)
 
     # Issues latency of one second
-    time.sleep(1)
+    time.sleep(latency_value_second)
     packet.accept()
 
+
+def packet_loss(packet):
+    # random value from 1 to 100
+    random_value = random.uniform(1, 100)
+
+    # If the generated value is smaller than the percentage discard
+    if packet_loss_percentage > random_value:
+        packet.drop()
+        print("[!] Packet dropped!")
+    # Accept the packet
+    else:
+        packet.accept()
+
+
+# -------VARIABLES---------- #
+mode = packet_loss  # <-- Change this variable to change the network degradation type
+
+# Degradation characteristic
+packet_loss_percentage = 10
+latency_value_second = 1
+# -------------------------- #
 
 # Creates the object
 nfqueue = NetfilterQueue()
 
-# 0 is the default NFQUEUE and print_packet is the
-nfqueue.bind(0, latency)
+# 0 is the default NFQUEUE
+nfqueue.bind(0, mode)
 
 try:
+    # Prints the mode the program is running in
+    print("[*] Mode is: ", end='')
+    print(mode.__name__)
+
+    # Shows the start waiting message
     print("[*] Waiting ")
     nfqueue.run()
 except KeyboardInterrupt:
