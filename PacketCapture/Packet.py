209f3919ac78069bf7ff99fcbaf34bf7d9401aa1
Aidan Fray <afray@hotmail.co.uk>
2017-11-03 15:17:27 +0000

    Changes; -Tidied code in the Packet.py file, added function comments an 'pythonised' some code to read easier - "-i" option has been added as a ignore mode, this is to check the speed at which packets can move through the NFQUEUE

--- PacketCapture/Packet.py
+++ PacketCapture/Packet.py
@@ -11,21 +11,27 @@ To restore full internet connection run:
         "sudo iptables -F"
 """
 
-# Default Vars
-latency_value_second = 0
-packet_loss_percentage = 0
-#
+def ignore_packet(packet):
+    """This is just used a default 'do nothing' function """
+
+    packet.accept()
 
 
 def print_packet(packet):
+    """This function just prints the packet"""
+
     print("[!] ", end='')
     print(packet)
     packet.accept()
 
 
 def edit_packet(packet):
+    """This function will be used to edit sections of a packet, this is currently incomplete"""
+
     # Converts into Scapy compatible string
     pkt = IP(packet.get_payload())
+
+    # TODO: Packet editing here
 
     # Sets the packet to the modified version
     packet.set_payload(bytes(pkt))
@@ -35,8 +41,10 @@ def edit_packet(packet):
 
 
 def packet_latency(packet):
-    # Used to debug
-    print_packet(packet)
+    """This function is used to incur latency on packets"""
+
+    # Shows the packet
+    print(packet)
 
     # Issues latency of the entered value
     time.sleep(latency_value_second)
@@ -45,6 +53,10 @@ def packet_latency(packet):
 
 
 def packet_loss(packet):
+    """This function will issue a packet loss,
+    a percentage is defined and anything
+    lower is dropped and anything else is accepted"""
+
     # random value from 1 to 100
     random_value = random.uniform(1, 100)
 
@@ -58,6 +70,8 @@ def packet_loss(packet):
 
 
 def run_packet_manipulation():
+    """The main method here, will issue a iptables command and construct the NFQUEUE"""
+
     try:
         # Runs the IPTABLES command
         os.system("iptables -A INPUT -j NFQUEUE")
@@ -73,80 +87,98 @@ def run_packet_manipulation():
         nfqueue.run()
     except (KeyboardInterrupt, SystemExit):
         print("\n[!] Process aborted")
+        print("[!] iptables reverted")
         os.system("iptables -F")
-        print("[!] iptables refreshed")
-        print("[*] Waiting")
 
 
 def help_message():
+    """Issues a terminal help message"""
+
     print("""
-Options:
-#=================================================#
-|-p                             - Print packet    |
-|-e                             - Packet edit     |
-|-l <latency_seconds>           - Latency         |
-|-pl <loss_percentage>          - Packet loss     |
-|-h                             - Help            |
-#=================================================#
-""")
+    Options:
+    #=================================================#
+    |-p                             - Print packet    |
+    |-e                             - Packet edit     |
+    |-l <latency_seconds>           - Latency         |
+    |-pl <loss_percentage>          - Packet loss     |
+    |-h                             - Help            |
+    #=================================================#
+    """)
 
 
-"""
-Parameter format:
-                  (0)      (1)         (2)
-    sudo python Packet.py <mode> <value_for_mode>
-    
-"""
-# Default
-mode = print_packet
-run = True
-mode_arg = ""
+def parameters():
+    """This function deals with the actions attached to parameters"""
 
-# Assigns parameter values
-if len(sys.argv) == 1:
-    print("Not enough parameters!")
-    help_message()
+    global mode
+    global latency_value_second
+    global packet_loss_percentage
 
-    # Stops it from running
-    run = False
-else:
-    mode_arg = str(sys.argv[1])
+    # --- default vars --- #
+    mode_arg = ""
+    mode_value_arg = ""
+    run = True
+    # -------------------- #
 
-# Assigning second parameter value
-mode_value_arg = 0
-if len(sys.argv) > 2:
-    mode_value_arg = str(sys.argv[2])
+    """Assigning Parameters"""
+    # Note: The script name i.e "Python.py" is counted as a parameter
+    length = len(sys.argv)
+    if length == 3:
+        mode_arg = str(sys.argv[1])
+        mode_value_arg = str(sys.argv[2])
 
-# ------SWITCH CASE FOR PARAMETERS PASSED -----#
-if mode_arg == "-p":
-    mode = print_packet
+    elif length == 2:
+        mode_arg = str(sys.argv[1])
 
-elif mode_arg == "-e":
-    mode = edit_packet
+    elif length == 1:
+        print("Not enough parameters!")
+        help_message()
+        run = False
 
-elif mode_arg == "-l":
-    mode = packet_latency
-    latency_value_second = int(mode_value_arg) / 1000
+    """Parameter List"""
+    if mode_arg == "-p":
+        mode = print_packet
 
-elif mode_arg == "-pl":
-    mode = packet_loss
-    packet_loss_percentage = int(mode_value_arg)
+    elif mode_arg == "-e":
+        mode = edit_packet
 
-elif mode_arg == "-h":
-    help_message()
-    run = False
+    elif mode_arg == "-l":
+        mode = packet_latency
+        latency_value_second = int(mode_value_arg) / 1000
 
-# No parameters
-elif mode_arg == "":
-    print()
+    elif mode_arg == "-pl":
+        mode = packet_loss
+        packet_loss_percentage = int(mode_value_arg)
 
-# Invalid parameters
-else:
-    print("Unsupported parameters included")
-    help_message()
-    run = False
-# ----------------------------------------------#
+    elif mode_arg == "-h":
+        help_message()
+        run = False
 
-# If everything is valid run == true
-if run:
-    run_packet_manipulation()
+    elif mode_arg == "-i":
+        mode = ignore_packet
+
+    #       <--- Add more parameters here
+
+    # No parameters
+    elif mode_arg == "":
+        print()
+
+    # Invalid parameters
+    else:
+        print("Unsupported parameters included")
+        help_message()
+        run = False
+
+    # If everything is valid run == true
+    if run:
+        run_packet_manipulation()
+
+# ------------------------DEFINITION END ------------------------------ #
+
+
+# Default value
+packet_loss_percentage = 0
+latency_value_second = 0
+mode = ignore_packet
+
+# Parameter handling
+parameters()
