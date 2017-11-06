0262ded772f7776db44d2515ef2af1dc9d72e146
Aidan Fray <afray@hotmail.co.uk>
2017-11-06 14:49:01 +0000

    Changes; -Added UDP to transfer a file from server to client

---
+++ ClientServer/ClientServer/UDP/UDPServer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ClientServer.UDP
+{
+    class UDPServer
+    {
+        //# Setup
+        UdpClient Server = new UdpClient(port);
+        public static int port = 1000;
+
+        Image UDP_Image;
+
+        //# Constructor 
+        public UDPServer()
+        {
+           
+        }
+
+        public async Task Start()
+        {
+            try
+            {
+                int _port = port; 
+
+                //IPEndPoint object will allow us to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(0, 0);
+                
+                // Blocks until a message returns on this socket from a remote host.
+                byte[] receiveBytes = Server.Receive(ref RemoteIpEndPoint);
+
+                //TODO: Need to add a loop here to receive multiple datagrams
+
+                //saves the image
+                UDP_Image = BytesToImage(receiveBytes);
+                UDP_Image.Save("image.png");
+
+                //END TODO: Make multi request?
+                Server.Close();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            
+        }
+
+        /// <summary>
+        /// Function used to convert a byte array into an Image
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        private Image BytesToImage(byte[] bytes)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (Image)converter.ConvertFrom((byte[])bytes);
+        }
+    }
+}
---
+++ ClientServer/ClientServer/UDP/UDPServer.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ClientServer.UDP
+{
+    /// <summary>
+    /// Interaction logic for UDPServer.xaml
+    /// </summary>
+    public partial class UDPServerWindow : Window
+    {
+        //# Connection definition
+        UDPServer server = new UDPServer();
+
+        //# Constructor
+        public UDPServerWindow()
+        {
+            InitializeComponent();
+        }
+
+        //# Functions
+        private void StartServer()
+        {
+            //# Starts the servers
+            Task t = Task.Factory.StartNew(() => server.Start());
+            t.ContinueWith((prev) => LoadPhoto());
+        }
+
+        /// <summary>
+        /// Used to load the photo after a transfer
+        /// </summary>
+        private void LoadPhoto()
+        {
+            //# Creates a bitmap image from file
+            BitmapImage bitmap = new BitmapImage(new Uri(@"image.png", UriKind.Relative));
+
+            //This make the bitmap image UI safe
+            bitmap.Freeze();
+
+            //# Needed to stop 
+            Image_TransferedImage.Dispatcher.Invoke(() => Image_TransferedImage.Source = bitmap);
+        }
+
+        //# Buttons 
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            StartServer();
+            Button_Start.Content = "STARTED";
+            Button_Start.IsEnabled = false;
+        }
+    }
+}
