4fd544c906e4a612160a6c712401001c19850811
Aidan Fray <afray@hotmail.co.uk>
2018-02-02 12:20:30 +0000

    Changes; - Added the ability to test multiple values of different effects with a single command. For example "-l 10-100-10" will test latency from values of 10-100 in steps of 10

--- PacketCapture/Experimentation/DownloadSpeedTests.py
+++ PacketCapture/Experimentation/DownloadSpeedTests.py
@@ -29,6 +29,8 @@ def run_degradation_script(parameters):
     global PROCESS
     PROCESS = None
 
+    start_time = time.time()
+
     print('[*] Running degradation script')
 
     script_name = "/Packet.py"
@@ -51,6 +53,16 @@ def run_degradation_script(parameters):
 
     PROCESS = subprocess.Popen(cmd, stdout=open('log.txt', 'w'))
 
+    # Speed code
+    grab_speeds()
+    clean_close('', '')
+
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+
+    print('[!] Test complete - Elapsed Time: {:.2f} seconds'.format(elapsed_time))
+
 
 def grab_speeds():
     """Talks to the speedtest.py script to grab upload and download speeds"""
@@ -68,33 +80,98 @@ def grab_speeds():
     UPLOAD = upload_speed_in_Mbps
 
 
-def print_speeds(download, upload):
+def print_speeds():
     print(' ' * 50, end='\r')
-    print("\n[!] ### Download: {:.2f}Mbps - Upload: {:.2f}Mbps ### [!]\n".format(download, upload))
+    print("[!] Download: {:.2f}Mbps - Upload: {:.2f}Mbps".format(DOWNLOAD, UPLOAD))
 
-try:
 
+def save_csv(x_data, y_data):
+    pass
+
+
+# TODO: This method needs validation
+def get_parameters():
     # Grabs all the parameters
     # Note: No validation
     parameters = sys.argv[1:]
 
-    # Removes the arguments so the speed script doesn't take them as well
-    del sys.argv[1:]
-
-    # No parameters defaults as just a connection speed tets
     if len(parameters) > 1:
-        run_degradation_script(parameters)
+
+        values = parameters[1:]
+
+        possible_range_value = values[-1].split('-')
+
+        # If it is a range parameter
+        if len(possible_range_value) > 1:
+
+            #   Format:
+            #       <start> <dash> <end> <dash> <step>
+            #   e.g.
+            #       10 - 100 - 2
+
+            # Nicer naming
+            range_values = possible_range_value
+
+            new_range = range(int(range_values[0]), int(range_values[1]), int(range_values[2]))
+
+            parameters.append(new_range)
+
+            return parameters
+        # If it's a regular parameter
+        else:
+            return parameters
+
+    # No parameters defaults as just a connection speed test
     else:
         print('[*] Just testing network speeds')
+        return None
 
-    grab_speeds()
-    clean_close('', '')
 
-    # Waits for all other messages to come through
-    time.sleep(0.5)
+def multiple_tests(parameters):
+    download_values = []
+    upload_vlaues = []
+    script_parameter = []
 
-    print_speeds(DOWNLOAD, UPLOAD)
-    sys.exit(0)
+    range_of_values = parameters[-1]
+    print('[*] Tests will be performed on this range: \'{}\''.format(range_of_values))
+
+    for value in range_of_values:
+
+        test = [parameters[0], str(value)]
+
+        print('\n[!] New test - Type: \'{}\' - Effect level: \'{}\''.format(test[0], test[1]))
+
+        run_degradation_script(test)
+
+        # Saves data to a series of lists
+        script_parameter.append(test)
+        download_values.append(DOWNLOAD)
+        upload_vlaues.append(UPLOAD)
+
+        print_speeds()
+
+    print(download_values)
+    print(upload_vlaues)
+
+
+try:
+
+    # TODO: Add a way for the script to test multiple different parameters and save the values to a .csv
+
+    parameters = get_parameters()
+    del sys.argv[1:]  # Stop other scripts from grabbing already used parameters
+
+    # Checks if the output is range
+    if isinstance(parameters[-1], range):
+        print('[!] Multiple test mode activated!')
+        multiple_tests(parameters)
+
+    elif parameters is not None:
+        run_degradation_script(parameters)
+
+        time.sleep(0.5)
+        print_speeds()
+
 
 except Exception as e:
     print('Error', e)
